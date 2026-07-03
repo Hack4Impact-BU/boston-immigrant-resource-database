398336b3fa6560e92a8ce63acc724907acc3e79e
Beren Akpinar <akpinarbe22@gmail.com>
2026-07-03 09:12:54 -0400

    Restore Sanity studio schemas and pin Node 20 for local dev.

---
+++ studio-unite-boston/unite-boston/schemaTypes/index.ts
@@ -0,0 +1,39 @@
+import stat from './stat';
+import partnerLogo from './partnerLogo';
+import testimonial from './testimonial';
+import faqItem from './faqItem';
+import menuItem from './menuItem';
+import aboutSection from './aboutSection';
+import partnersSection from './partnersSection';
+import footerSettings from './footerSettings';
+import headerSettings from './headerSettings';
+import imageBanner from './imageBanner';
+import contactSettings from './contactSettings';
+import footerColumn from './footerColumn';
+import navLink from './navLink';
+import footerLink from './footerLink';
+import forumPost from './forumPost';
+import forumComment from './forumComment';
+import forumSettings from './forumSettings';
+import resourceSection from './resourceSection';
+
+export const schemaTypes = [
+  stat,
+  partnerLogo,
+  testimonial,
+  faqItem,
+  menuItem,
+  aboutSection,
+  partnersSection,
+  footerSettings,
+  headerSettings,
+  imageBanner,
+  contactSettings,
+  footerColumn,
+  navLink,
+  footerLink,
+  forumPost,
+  forumComment,
+  forumSettings,
+  resourceSection,
+];
