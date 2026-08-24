1d24f118733d1cf9fb21bf65b31288b64f7da100
rtangjuiced <145133817+rtangjuiced@users.noreply.github.com>
2026-08-24 18:12:05 -0400

    commenting out repetitive ImageBanner for now

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <main>
         <AboutBirdSection />
         <WhyBirdMattersSection />
-        <ImageBanner />
+        {/* <ImageBanner /> */}
         <OurPartnersSection />
         <SponsorLogos />
         {/* <TestimonialsSection /> */}
